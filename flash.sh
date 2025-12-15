3d983b0d0e1161bf3d702a8e864e6a0d5264e090
TheGBO <gabrielthejota@gmail.com>
2025-12-15 18:36:03 -0300

    padroniza y/n

--- flash.sh
+++ flash.sh
@@ -2,7 +2,7 @@
 
 echo "ATENÇÃO, isso pode FODER COMPLETAMENTE qualquer dispositivo (e os dados) em /dev/sdb"
 echo "Digite 'lsblk' para ter certeza do dispositivo"
-read -r -p "Tem certeza do que está fazendo? [s/n]" response
+read -r -p "Tem certeza do que está fazendo? [y/n]" response
 
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
